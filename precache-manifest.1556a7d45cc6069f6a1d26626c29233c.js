3dee743e7cc6c8e750d949468f39f8e709917350
serhii <romanchuk.sss22121999@gmail.com>
2020-12-13 16:21:14 +0200

    Updates

---
+++ precache-manifest.1556a7d45cc6069f6a1d26626c29233c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b68962248a643a6baccda5b813a9f09",
+    "url": "/simple-react-tasks/index.html"
+  },
+  {
+    "revision": "2c84c641320547f92afb",
+    "url": "/simple-react-tasks/static/css/main.212b25b4.chunk.css"
+  },
+  {
+    "revision": "73bb2c98156fb4c2f0ab",
+    "url": "/simple-react-tasks/static/js/2.43ed494c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-tasks/static/js/2.43ed494c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c84c641320547f92afb",
+    "url": "/simple-react-tasks/static/js/main.cf4c14dc.chunk.js"
+  },
+  {
+    "revision": "f395941d6ff83486dc91",
+    "url": "/simple-react-tasks/static/js/runtime-main.bad636bd.js"
+  }
+]);
