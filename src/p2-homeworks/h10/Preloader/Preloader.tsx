cfd7921b0d347fca5043a0bc7f1e64c3784bddee
serhii <romanchuk.sss22121999@gmail.com>
2021-01-17 20:02:29 +0200

    10 Preloader v2.1

--- src/p2-homeworks/h10/Preloader/Preloader.tsx
+++ src/p2-homeworks/h10/Preloader/Preloader.tsx
@@ -3,12 +3,9 @@ import classes from './Preloader.module.css';
 
 export const Preloader = () => {
    return (
-      <React.Fragment>
-         <div className={classes.loaderContainer}>
-            <div className={classes.ldsHourglass}></div>
-            <div className={classes.textLoading}>Loading...</div>
-         </div>
-
-      </React.Fragment>
+      <div className={classes.loaderContainer}>
+         <div className={classes.ldsHourglass}></div>
+         <div className={classes.textLoading}>Loading...</div>
+      </div>
    );
 }
